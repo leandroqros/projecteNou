9383e3cad7da0a6580039b678ab910115dc626e1
Leandro Queiroz <leandroqros@gmail.com>
2022-05-27 15:43:36 +0200

    Creado boton Leandro

--- projecteNou/Form1.Designer.cs
+++ projecteNou/Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace projecteNou
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btLeandro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btLeandro
+            // 
+            this.btLeandro.Location = new System.Drawing.Point(46, 41);
+            this.btLeandro.Name = "btLeandro";
+            this.btLeandro.Size = new System.Drawing.Size(75, 23);
+            this.btLeandro.TabIndex = 0;
+            this.btLeandro.Text = "button1";
+            this.btLeandro.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btLeandro);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btLeandro;
     }
 }
 
